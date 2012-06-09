5926c144ee2c3eee7ba7da15b783d921734af0fb
Peter Philipp <peter.philipp@cando-image.com>
2012-06-09 18:44:33 +0200

    Updated theme.

--- themes/wanderitis_theme/panels/layouts/wanderitis/wanderitis/wanderitis.tpl.php
+++ themes/wanderitis_theme/panels/layouts/wanderitis/wanderitis/wanderitis.tpl.php
@@ -6,6 +6,9 @@
     $middle_column_grid = 12;
     $left_column_grid = 4;
   }
+if (empty($content['left'])) {
+  $middle_column_grid = 12;
+}
 ?>
 
 <div class="panel-display omega-grid omega-16-threecol-2-10-4" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
@@ -19,15 +22,19 @@
       <div class="inside"><?php print $content['top']; ?></div>
     </div>
   </div>
-  <div class="panel-panel grid-<?php print $left_column_grid ?>">
+<?php if (!empty($content['left'])) : ?>
+  <div class="panel-panel grid-<?php print $left_column_grid ?> left">
     <div class="inside"><?php print $content['left']; ?></div>
   </div>
-  <div class="panel-panel grid-<?php print $middle_column_grid ?>">
+<?php endif; ?>
+  <div class="panel-panel grid-<?php print $middle_column_grid ?> middle">
     <div class="inside"><?php print $content['middle']; ?></div>
   </div>
-  <div class="panel-panel grid-4">
+<?php if (!empty($content['right'])) : ?>
+  <div class="panel-panel grid-4 right">
     <div class="inside"><?php print $content['right']; ?></div>
   </div>
+<?php endif; ?>
   <div class="panel-panel grid-16 footer">
     <div class="inside"><?php print $content['bottom']; ?></div>
   </div>
