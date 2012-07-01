792ead8c927f5b676453af485bc979244b9c99d7
Peter Philipp <peter.philipp@cando-image.com>
2012-07-01 09:29:46 +0200

    Updated theme.

--- themes/wanderitis_theme/panels/layouts/wanderitis/wanderitis/wanderitis.tpl.php
+++ themes/wanderitis_theme/panels/layouts/wanderitis/wanderitis/wanderitis.tpl.php
@@ -1,27 +1,33 @@
 <?php
   // Smart theme reacts on available content.
+  $overall_grid_class = 'omega-16-threecol-2-10-4';
   $middle_column_grid = 10;
   $left_column_grid = 2;
+  $right_column_grid = 4;
   if (empty($content['right'])) {
     $middle_column_grid = 12;
     $left_column_grid = 4;
+    $overall_grid_class = 'omega-16-twocol-4-12';
   }
-if (empty($content['left'])) {
-  $middle_column_grid = 12;
-}
+  if (empty($content['left'])) {
+    $middle_column_grid = 12;
+    $right_column_grid = 4;
+    $overall_grid_class = 'omega-16-twocol-12-4';
+  }
 ?>
 
-<div class="panel-display omega-grid omega-16-threecol-2-10-4" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel grid-16 header-float">
+<div class="panel-display omega-grid <?php print $overall_grid_class; ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-region grid-16 header-float">
     <div class="panel-panel grid-16 top-left">
       <div class="inside"><?php print $content['top_left']; ?></div>
     </div>
   </div>
-  <div class="panel-panel grid-16 header">
+  <div class="panel-region grid-16 header">
     <div class="panel-panel grid-16 top">
       <div class="inside"><?php print $content['top']; ?></div>
     </div>
   </div>
+  <div class="panel-region grid-16 content">
 <?php if (!empty($content['left'])) : ?>
   <div class="panel-panel grid-<?php print $left_column_grid ?> left">
     <div class="inside"><?php print $content['left']; ?></div>
@@ -31,11 +37,12 @@ if (empty($content['left'])) {
     <div class="inside"><?php print $content['middle']; ?></div>
   </div>
 <?php if (!empty($content['right'])) : ?>
-  <div class="panel-panel grid-4 right">
+  <div class="panel-panel grid-<?php print $right_column_grid ?> right">
     <div class="inside"><?php print $content['right']; ?></div>
   </div>
 <?php endif; ?>
-  <div class="panel-panel grid-16 footer">
+  </div>
+  <div class="panel-region grid-16 footer">
     <div class="inside"><?php print $content['bottom']; ?></div>
   </div>
 </div>
