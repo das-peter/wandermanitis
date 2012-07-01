0969c90d632f41d41ae543b389ea9561c889ee17
Peter Philipp <peter.philipp@cando-image.com>
2012-07-01 15:12:27 +0200

    Updates theme.

--- themes/wanderitis_theme/template.php
+++ themes/wanderitis_theme/template.php
@@ -16,3 +16,41 @@ function wanderitis_theme_links__locale_block ($vars) {
   unset($vars['links']['en']);
   return '<span class="language-switcher-title">' . t('Languages') . ':</span> ' . theme('links', $vars);
 }
+
+/**
+ * Returns HTML for an inactive facet item.
+ *
+ * @param $variables
+ *   An associative array containing the keys 'text', 'path', and 'options'. See
+ *   the l() function for information about these variables.
+ *
+ */
+function wanderitis_theme_facetapi_link_active($variables) {
+
+  // Sanitizes the link text if necessary.
+  $sanitize = empty($variables['options']['html']);
+  $link_text = ($sanitize) ? check_plain($variables['text']) : $variables['text'];
+
+  // Theme function variables fro accessible markup.
+  // @see http://drupal.org/node/1316580
+  $accessible_vars = array(
+    'text' => $variables['text'],
+    'active' => TRUE,
+  );
+
+  // Builds link, passes through t() which gives us the ability to change the
+  // position of the widget on a per-language basis.
+  $replacements = array(
+    '!facetapi_deactivate_widget' => theme('facetapi_deactivate_widget'),
+    '!facetapi_accessible_markup' => theme('facetapi_accessible_markup', $accessible_vars),
+  );
+  $variables['text'] = t('!facetapi_deactivate_widget !facetapi_accessible_markup', $replacements);
+  $variables['options']['html'] = TRUE;
+
+  // Do some stupid stuff to ensure the link is clickable.
+  $text_variables = $variables;
+  $text_variables['text'] = $link_text;
+  unset($text_variables['options']['attributes']['class'][1]);
+
+  return theme_link($variables) . theme_link($text_variables);
+}
